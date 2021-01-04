fce8758b0b2e998133fb4bf9c569903c0a0d20b3
Zaidov012 <zaidov012@gmail.com>
2021-01-04 21:25:39 +0400

    V. 1.0

---
+++ MyTestApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("TESTING GitHub");
+        }
+    }
+}
